b72ceed54d30e78143ab5febab27bd4a04ef7277
cag2025 <cag.22ex23001@gmail.com>
2025-08-19 12:44:23 +0530

    Completed 12th august nicely

--- Aug 12/my-answer.sh
+++ Aug 12/my-answer.sh
@@ -6,10 +6,10 @@ gmt gmtset FONT_LABEL Times-Roman
 
 # Region of South America
 # Corrected the format for the whole world
-Reg=-R80/90/20/35
+Reg=-R83/90/25/30
 
 # Projection (mercator) with a new, smaller scale to fit the page
-Pro=-Jm0.5
+Pro=-Jm3
 
 # Input grid and color palettes
 GRD=~/Desktop/topo30.grd
@@ -22,7 +22,7 @@ ps=answer.ps
 gmt grdcut $GRD $Reg -Gtmp1.grd -V
 
 # Step 2: Resample to 4 arc-minute resolution
-gmt grdsample tmp1.grd -Gtmp.grd -I4m -r -nc+c -V
+gmt grdsample tmp1.grd -Gtmp.grd -I0.5m -r -nc+c -V
 
 # Step 3: Compute gradient for shading
 gmt grdgradient tmp.grd -A315 -Ggradient.grd -Nt -V
@@ -37,12 +37,14 @@ gmt grdimage tmp.grd $Reg $Pro -C$CPT -Igradient.grd -O -K >> $ps
 # This command plots both permanent major (-Ia) and minor (-Ib) rivers.
 gmt pscoast $Reg $Pro -Q -W0.25p,black -Ia/0.5,blue -O -K >> $ps
 # Step 7: Plot the station (27.34 85.99) as a filled red inverted triangle
-echo "85.99 27.34" | gmt psxy $Reg $Pro -Si0.1i -Gred -W0.5p,black -O -K >> $ps
+echo "85.99 27.34" | gmt psxy $Reg $Pro -Si0.5i -Gred -W3p,red -O -K >> $ps
 
 # Step 8: Plot the locations from location.dat as filled blue circles
-awk '{print $2, $1}' location.dat | gmt psxy $Reg $Pro -S+0.2i -Gblue -Wthin -O >> $ps
+awk '{print $2, $1}' location.dat | gmt psxy $Reg $Pro -Sx0.5i -Gblue -W3,purple -O >> $ps
 # Step 8: Label the cities
 # gmt pstext cities.dat $Reg $Pro -F+f10p,Times-Roman+jRT -D0.1i/0.1i -O -K >> $ps
+
+okular answer.ps
 
 # Step 9: Finalize the PostScript file
 # gmt gmtshow $ps
